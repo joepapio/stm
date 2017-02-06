2e4c09596516b4428282ae84727258ae372b3d3f
Joe Papio <Joe Papio>
2017-02-06 09:25:38 -0600

    updated function to extract vcov values and store with betas in a tidy(ish) dataframe

--- Prez.R
+++ Prez.R
@@ -191,54 +191,136 @@ map(FX.test[[1]][[1]], length)
 map(FX.Prez20.all[[1]], length)
 
 
+  
+
 
 
 grabEst <-function(FX){
   #Function to pull parameter estimates from object produced by stm estimateEffects object
-  #will automatically 
   
- res <- NULL  ##results storage object
+  ## ensure that object is stm estimateEffect object, otherwise give error
+ if(!length(FX)==8){
+   stop(cat("input is not an stm estimateEffect"))
+ }else{
+  if(!attributes(FX)$class=="estimateEffect"){
+    stop(cat("input is not an stm estimateEffect"))
+  }else{
+  
+    ##start function if object is of correct type
+    
+  ##create null objects for results storage
+ parm <- NULL  
+ vcvm <- rep(list(NULL),length(FX[[2]]))
+ vv <- NULL
  
- ###for loop to extract simulations from estimateEffect object
+ ###for loop to extract betas and stdevs from estimateEffect object for the i-th topic
    for (i in 1:length(FX[[2]])){
-  res1 <-   transpose(FX[[1]][[i]])$est %>% ##use purrr tranpose function to gather all parameter 
+  parm1 <-   transpose(FX[[1]][[i]])$est %>% ##use purrr tranpose function to gather all parameter 
                                             ##estimates together
       transpose() %>% ### use purrr transpose function to gather similar simulation values together together
       melt() %>%   ### use reshape2 melt funcion to turn list into data.frame
       group_by(L1) %>% ## use dplyr group_by function to group separate parameter simulations
-      summarize(   ### use dplyr summarize function to find mean of simulations for eachparameter
+      summarize(   ### use dplyr summarize function to find mean of simulations for each parameter
         Topic =mean(value)
       )
-  if(is.null(res)){
+  if(is.null(parm)){
     ##for first topic, store results
-    res <- res1
-    names(res)[i+1] <- paste( "Topic", i, sep="" ) 
+    parm <- parm1
+    names(parm)[i+1] <- paste( "Topic", i, sep="" ) 
   } else{
     ##for subsequent topics, store results
-    res <- merge(res, res1, by="L1") 
-    names(res)[i+1] <- paste( "Topic", i, sep="" )   
+    parm <- merge(parm, parm1, by="L1") 
+    names(parm)[i+1] <- paste( "Topic", i, sep="" )   
   }
+  ##nested forloop to  extract variances for the j-th simulation
+    for (j in 1:length(transpose(FX[[1]][[1]])$vcov)){
+      if(is.null(vcvm[[i]])){
+        #v-cov matrix for first simulation 
+        vcvm[[i]] <-  (transpose(FX[[1]][[i]])$vcov)[[j]]    
+      }else{
+        #add v-cov matrix for the j-th simulation
+        vcvm[[i]] <- vcvm[[i]] + (transpose(FX[[1]][[i]])$vcov)[[j]]
+      }
+    }
+  ##multiply by inverse of number of simluations squared for i-th topic
+    vcvm[[i]]<- vcvm[[i]]*(1/length(transpose(FX[[1]][[1]])$vcov))^2
+    if(is.null(vv)){
+      ##for first topic, set up storage object, getting first column from parms object so names match
+      vv<- as.data.frame(parm[,1])
+    }
+    #extract square root of diagonal elements for i-th topic
+      vv<- cbind(vv,sqrt(diag(vcvm[[i]])) )  
+    
+    #label column as the i-th topic
+    names(vv)[i+1] <- paste( "Topic", i, sep="" )
    }
+ 
+ 
+ ###combine storage object
+ 
  ##rename first column
- names(res)[1] <- "Parameter"
+ names(parm)[1] <- "Parameter"
+ names(vv)[1] <- "Parameter"
+ 
+ stdev <- vv %>% melt() %>% rename(sd=value)
+ parms <- parm %>% melt() %>% rename(beta=value)
+ 
+ res <- cbind(parms, stdev[,3])
+ names(res)[4] <- "sd"
+ 
  ##print results
  res
-}
+  }
+   }
+ } 
 
-works <- grabEst(FX.Prez20.all)
 
+works <- grabEst(FX.)
+works
 
 dim(summary)
 
-grabEst(FX.Prez0a.all)
+ests <- grabEst(FX.Prez20.noints)
 
- <- function(FX){
-  factors <- NULL
-  for (i in 1:length(FX$varlist){
-    factors[i]  <- dim(summary(FX.$data[i]))[1]
+head(ests)
+     
+
+FX.Prez20.noint <- estimateEffect(1:20 ~0+s(year)+party+feel, stmFit.Prez20, meta=stm.Prez$meta)
+
+FX.Prez20.noints <- estimateEffect(1:20 ~0+s(year)+0+party+0+feel, stmFit.Prez20, meta=stm.Prez$meta)
+
+
+
+
+
+vcvm <- rep(list(NULL),length(FX[[2]]))
+vv<- as.data.frame(parm[,1])
+for (i in 1:length(FX[[2]])){
+for (j in 1:length(transpose(FX[[1]][[1]])$vcov)){
+  if(is.null(vcvm[[i]])){
+  vcvm[[i]] <-  (transpose(FX[[1]][[i]])$vcov)[[j]]    
+  }else{
+    vcvm[[i]] <- vcvm[[i]] + (transpose(FX[[1]][[i]])$vcov)[[j]]
   }
 }
+vcvm[[i]]<- vcvm[[i]]*(1/length(transpose(FX[[1]][[1]])$vcov))^2
+ vv<- cbind(vv,sqrt(diag(vcvm[[i]])) )
+ names(vv)[i+1] <- paste( "Topic", i, sep="" )
+}
+names(vv)[1] <- "Parameter"
+
+
+stdev <- vv %>% melt() %>% rename(sd=value)
+betas <- parm %>% melt() %>% rename(parms=value)
   
-     
-     
-     
+res <- cbind(betas, stdev[,3])
+names(res)[4] <- "sd"
+
+
+
+plot.estimateEffect(FX.Prez20.noints, 
+                    covariate="year", 
+                    method="continuous", 
+                    topics =c(1:14))
+
+
